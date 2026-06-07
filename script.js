d7ff24d1f5b60d3355cd55c376a01dc7c26bd5bc
juanile <juanile97@gmail.com>
2026-06-06 21:08:31 -0300

    final casi terminado

--- script.js
+++ script.js
@@ -11,3 +11,42 @@ function toggleInfo(id){
     }
 
 }
+
+const carrusel = document.querySelector(".carrusel-contenedor");
+const imagenes = document.querySelectorAll(".carrusel-contenedor img");
+
+let indice = 0;
+
+function mostrarImagen(){
+
+    imagenes[indice].scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "nearest"
+    });
+
+}
+
+document.querySelector(".next").addEventListener("click", () => {
+
+    indice++;
+
+    if(indice >= imagenes.length){
+        indice = 0;
+    }
+
+    mostrarImagen();
+
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+
+    indice--;
+
+    if(indice < 0){
+        indice = imagenes.length - 1;
+    }
+
+    mostrarImagen();
+
+});
